d34956ec7f80fbaa4ca30c2e16e7989a2dd81b96
Josh Minnie <josh.minnie@gmail.com>
2021-08-04 10:49:59 -0400

    Add analysis alias

--- plugins/hubtrics/hubtrics.plugin.zsh
+++ plugins/hubtrics/hubtrics.plugin.zsh
@@ -1,3 +1,5 @@
 hubtrics=~/Projects/hubtrics
 
 alias label="cd $hubtrics;bin/apply_labels --repository"
+alias analyze="cd $hubtrics;bin/analyze --repository"
+alias analyze_all="analyze verifyvalid/verifyvalid ; analyze verifyvalid/vv_registrar ; analyze verifyvalid/check_engine ; analyze verifyvalid/check_engine_pci ; analyze verifyvalid/vv_internal_api_client"
