a11e3009ff7828db7d8349412871c6ef6d5eecef
Josh Minnie <josh.minnie@gmail.com>
2022-11-17 10:12:48 -0500

    Enhance rails plugin - make the rails server command more robust - migrate project specific aliases

---
+++ plugins/dpx/dpx.plugin.zsh
@@ -0,0 +1,3 @@
+alias dj="bin/delayed_job"
+alias rc="bundle exec rails c"
+alias bup="bin/bup"
--- plugins/rails/rails.plugin.zsh
+++ plugins/rails/rails.plugin.zsh
@@ -1,25 +1,16 @@
 # Aliases for eChecks project
 alias gup="OVERCOMMIT_DISABLE=1 git up"
 alias ginit="git init;git add .;git commit -m 'Initial commit'"
-alias dj="bin/delayed_job"
 alias rc="bundle exec rails c"
-alias bup="bin/bup"
 
 function rs {
   local port="$1"
   [[ -z "$port" ]] && port=3000
-  if (( $+commands[passenger] )); then
-    echo "Starting passenger on port $port..."
-    passenger start -p "$port"
-  else
-    print -P '%F{yellow}'
-    cat <<-'EOF'
-  You need to install the passenger gem before you can run this command.
-
-  gem install passenger
-EOF
-    print -P '%f'
+  if [[ !(( $+commands[passenger] )) ]]; then
+    gem install passenger
   fi
+  echo "Starting passenger on port $port..."
+  passenger start -p "$port"
 }
 
 # Kills instances of passenger that are running.
